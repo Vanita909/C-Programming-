0c367e39e82e005158163b0b2b9962f6789d020d
Vanita909 <vaghelavanita76@gmail.com>
2026-09-24 09:58:34 +0530

    cpp

---
+++ BUBBLS~1.CPP
@@ -0,0 +1,39 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+    int a[5], i, j, temp;
+
+    clrscr();
+
+    cout << "Enter 5 values: ";
+
+    for(i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+
+    // Bubble Sort
+    for(i = 0; i < 4; i++)
+    {
+        for(j = 0; j < 4 - i; j++)
+        {
+            if(a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "\nSorted array: ";
+
+    for(i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    getch();
+}
