e2c4e497196bd00c9125f1380fe863151fa79930
Vanita909 <vaghelavanita76@gmail.com>
2026-09-24 11:05:13 +0530

    cpp

---
+++ HOWER.CPP
@@ -0,0 +1,35 @@
+#include <iostream.h>
+#include <conio.h>
+
+void hanoi(int n, char from, char to, char aux)
+{
+    if(n == 1)
+    {
+        cout << "Move disk 1 from " << from
+             << " to " << to << "\n";
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);
+
+    cout << "Move disk " << n << " from " << from
+         << " to " << to << "\n";
+
+    hanoi(n - 1, aux, to, from);
+}
+
+void main()
+{
+    int n;
+
+    clrscr();
+
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    cout << "\nSteps to solve Tower of Hanoi:\n";
+
+    hanoi(n, 'A', 'C', 'B');
+
+    getch();
+}
